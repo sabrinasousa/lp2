08158bee4799b1ba14f200d7908e7ed1dc114acf
sabrinasousa <sabrina.sousasilva@hotmail.com>
2015-03-16 10:28:17 -0300

    Create ProgramaJava2.java
    segundo programa de java

---
+++ aulas/ProgramaJava2.java
@@ -0,0 +1,8 @@
+public class ProgramaJava2{
+        public static void main(String[]args){
+                String parametro = args[0];
+
+                System.out.println("O valor digitado foi:" + parametro);
+
+        }
+}
