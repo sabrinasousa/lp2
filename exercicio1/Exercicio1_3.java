582543ebe9e2060592828a07eb5e28f34957f723
sabrinasousa <sabrina.sousasilva@hotmail.com>
2015-03-17 11:11:44 -0300

    Create Exercicio1_3.java
    Exercicio1

---
+++ exercicio1/Exercicio1_3.java
@@ -0,0 +1,15 @@
+public class Exercicio1_3{
+	public static void main (String[] args){
+		int a=45;
+		int b=20;
+		int c=3;
+		int soma = a + b + c;
+		int multiplicacao = a * b * c;
+		int subtracao = a - b - c;
+		int divisao = a / b / c;
+		System.out.println("o valor da soma foi: " + soma);
+		System.out.println("o vamor da multiplicação foi: " + multiplicacao);
+		System.out.println("o valor da divisao foi: " + divisao);
+		System.out.println("o valor da subtração foi: " + subtracao);
+	}
+}
