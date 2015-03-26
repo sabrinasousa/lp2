53430c53f4c0908ea70c5f5613318d77602834cd
sabrinasousa <sabrina.sousasilva@hotmail.com>
2015-03-26 15:22:30 -0300

    Create Exponencial.java
    exercicio 6 da pasta exercicio 2

---
+++ exercicio2/Exponencial.java
@@ -0,0 +1,12 @@
+public class Exponencial {
+
+    public static void main(String[] args) {
+       
+        System.out.println("# Q C");
+        
+        for(int n=0; n<=10; n++){
+            
+            System.out.println(n + " " + (n*n) + " " + (n*n*n));
+        }
+    }
+}
